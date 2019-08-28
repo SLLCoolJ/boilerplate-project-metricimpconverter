7f0ad18670365b9442f5aeb01e0e32217e81a1a8
SLLCoolJ <46657782+SLLCoolJ@users.noreply.github.com>
2019-08-28 13:16:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+const helmet    = require('helmet'); 
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet.noSniff());
+app.use(helmet.xssFilter())
+// app.use(helmet.hidePoweredBy({setTo:'PHP 4.2.0'}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -27,9 +32,13 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
+
+
 //Routing for API 
 apiRoutes(app);  
     
+
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
