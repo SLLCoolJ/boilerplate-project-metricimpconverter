12f0a78f1fbcbca3cf24f0cd58bd9c95cde5d923
SLLCoolJ <46657782+SLLCoolJ@users.noreply.github.com>
2019-08-28 13:14:35 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -19,12 +19,23 @@ module.exports = function (app) {
     .get(function (req, res){
       var input = req.query.input;
       var initNum = convertHandler.getNum(input);
+
       var initUnit = convertHandler.getUnit(input);
+    if(initNum==="Invalid Number"){
+      if(initUnit==="Invalid Unit"){
+      res.send('Invalid Number and Unit');
+      }
+      res.send('Invalid Number');
+      }
+    if(initUnit==="Invalid Unit"){
+      res.send('Invalid Unit');
+      }
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+
+    res.json({"initNum":initNum,"initUnit":initUnit,"returnNum":returnNum,"returnUnit":returnUnit,"string":toString});
+    
     });
     
 };
